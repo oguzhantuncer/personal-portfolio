d6d8d8c497390e71d9394d2f5a7e986430919bbd
Oğuzhan <oguzhantuncer@outlook.com.tr>
2023-06-01 01:22:52 +0300

    cv indirme butonu eklendi

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,16 +11,3 @@ $(document).ready(function(){
 function myFunction(x) {
     x.classList.toggle("change");
   }
-
-  var dosyaYukleBtn = document.getElementById("dosyaYukleBtn");
-  dosyaYukleBtn.addEventListener("click", function() {
-    // Dosya seçme iletişim kutusunu aç
-    var dosyaYukleInput = document.createElement("input");
-    dosyaYukleInput.type = "file";
-    dosyaYukleInput.addEventListener("change", function() {
-      var secilenDosya = dosyaYukleInput.files[0];
-      // Dosya yükleme işlemini burada gerçekleştir
-      console.log("Seçilen dosya: ", secilenDosya);
-    });
-    dosyaYukleInput.click();
-  });
