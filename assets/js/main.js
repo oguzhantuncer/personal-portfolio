d5c70a0ae94fbde7abc7ca43ca5b8db40ca338d0
Oğuzhan <oguzhantuncer@outlook.com.tr>
2023-07-07 00:39:50 +0300

    code edits

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@ function myFunction(x) {
   }
 
   // scroll-up button show/hide script
-  if(this.scrollY > 500){
+if(this.scrollY > 500){
     $('.scroll-up-btn').addClass("show");
 }else{
     $('.scroll-up-btn').removeClass("show");
@@ -37,40 +37,3 @@ $('.navbar .menu').toggleClass("active");
 $('.menu-btn i').toggleClass("active");
 });
 
-// typing text animation script
-var typed = new Typed(".typing", {
-strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-typeSpeed: 100,
-backSpeed: 60,
-loop: true
-});
-
-var typed = new Typed(".typing-2", {
-strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-typeSpeed: 100,
-backSpeed: 60,
-loop: true
-});
-
-// owl carousel script
-$('.carousel').owlCarousel({
-margin: 20,
-loop: true,
-autoplay: true,
-autoplayTimeOut: 2000,
-autoplayHoverPause: true,
-responsive: {
-    0:{
-        items: 1,
-        nav: false
-    },
-    600:{
-        items: 2,
-        nav: false
-    },
-    1000:{
-        items: 3,
-        nav: false
-    }
-}
-});
